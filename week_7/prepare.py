0804fc913b24e0b124e9a1187b4cda2443c04a5b
brainless-astronaut <ssahana1608@gmail.com>
2025-11-12 17:41:54 +0000

    feat: complete Week 7 pipeline and register model

--- week_7/prepare.py
+++ week_7/prepare.py
@@ -4,34 +4,32 @@ import yaml
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-## Read params
+# Read params
 params = yaml.safe_load(open("params.yaml"))["prepare"]
 split = params["split"]
 seed = params["seed"]
 
-## Create output directory
-os.makedirs("data/prepared", exist_ok = True)
+# Read input file path from command line
+input_file = sys.argv[1]
 
-## Define columns
-col_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'target']
+# Create output directory
+os.makedirs("data/prepared", exist_ok=True)
 
-## Reading the data
-df = pd.read_csv('data/raw/iris.csv', header = None, names = col_names)
-
-## Rename columns from "sepal.length" to "sepal_length"
+# Read the data, rename columns, and map text labels to numbers
+df = pd.read_csv(input_file)
 df.rename(columns={
     "sepal.length": "sepal_length",
     "sepal.width": "sepal_width",
     "petal.length": "petal_length",
     "petal.width": "petal_width",
     "variety": "target"
-}, inplace = True)
+}, inplace=True)
 
-## Mapping the string tagets to integers
-target_map = {'Iris-setosa': 0, 'Iris-versicolor': 1, 'Iris-virginica': 2}
+# Map string targets to integers
+target_map = {"Setosa": 0, "Versicolor": 1, "Virginica": 2}
 df['target'] = df['target'].map(target_map)
 
-## Split and save
+# Split and save
 train, test = train_test_split(df, test_size=split, random_state=seed)
 train.to_csv("data/prepared/train.csv", index=False)
 test.to_csv("data/prepared/test.csv", index=False)
